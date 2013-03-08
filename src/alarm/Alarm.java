79cbd3ff441c3f67a1b2f6a326e6538720bc5125
vinnu <vinnu@vinnu-HP-Mini-110-3500>
2013-03-08 16:47:24 +0530

    created

---
+++ src/alarm/Alarm.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alarm;
+
+/**
+ *
+ * @author vinnu
+ */
+public class Alarm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
